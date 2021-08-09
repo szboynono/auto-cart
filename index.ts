f5304fab18802d5748d8303d8afb5619233b363f
szboynono <szboynono@gmail.com>
2021-08-09 01:09:09 -0400

    done

--- index.ts
+++ index.ts
@@ -1,12 +1,55 @@
+import dotenv from 'dotenv';
 import puppeteer from 'puppeteer';
+import nodemailer from 'nodemailer';
+
+dotenv.config()
+
+const sendEmail = async () => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  await transporter.sendMail({
+    from: `"AUTOCART ALERT" <${process.env.EMAIL}>`, // sender address
+    to: "szboylbd@hotmail.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  }, (err) => {
+    if (err) {
+      console.log('err: ', err)
+    } else {
+      console.log('sent!')
+    }
+  });
+}
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox'] });
+    args: ['--no-sandbox']
+  });
   const page = await browser.newPage();
-  await page.goto('https://example.com');
-  await page.screenshot({ path: 'example.png' });
+  console.log('go to the page')
+  await page.goto('https://www.bestbuy.ca/en-ca/product/playstation-5-dualsense-wireless-controller-charging-station/14962276', { waitUntil: 'networkidle2' });
+
+  const [button] = await page.$x("//button[contains(., 'Add to Cart')][@disabled]");
+
+  
+
+  if (!button) {
+    const [activeButton] = await page.$x("//button[contains(., 'Add to Cart')]");
+    if (activeButton) {
+      sendEmail()
+      console.log('BUY IT NOW!!!')
+    }
+  } else {
+    console.log('button not enabled')
+  }
 
   await browser.close();
 })();
