8b20e73c83b9df63f6c4f446fbb0698c68d8e0a5
szboynono <szboylbd@hotmail.com>
2021-08-08 23:14:37 -0400

    setup done

---
+++ index.ts
@@ -0,0 +1,12 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox'] });
+  const page = await browser.newPage();
+  await page.goto('https://example.com');
+  await page.screenshot({ path: 'example.png' });
+
+  await browser.close();
+})();
