c363f0f1b31df98e1fe63fa5ab1879d494c63fb6
szboynono <szboylbd@hotmail.com>
2022-05-03 19:01:38 -0400

    well, now its a hermes bag bot

--- index.ts
+++ index.ts
@@ -1,55 +1,69 @@
-import dotenv from 'dotenv';
-import puppeteer from 'puppeteer';
-import nodemailer from 'nodemailer';
+import dotenv from "dotenv";
+import puppeteer from "puppeteer";
+import nodemailer from "nodemailer";
 
-dotenv.config()
+dotenv.config();
 
-const sendEmail = async () => {
+const sendEmail = async (html: string) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
       user: process.env.EMAIL,
       pass: process.env.PASSWORD,
     },
   });
 
-  await transporter.sendMail({
-    from: `"AUTOCART ALERT" <${process.env.EMAIL}>`, // sender address
-    to: "szboylbd@hotmail.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
-  }, (err) => {
-    if (err) {
-      console.log('err: ', err)
-    } else {
-      console.log('sent!')
+  await transporter.sendMail(
+    {
+      from: `"AUTOCART ALERT" <${process.env.EMAIL}>`, // sender address
+      to: "feilinfu729@gmail.com", // list of receivers
+      subject: "We got a hit ✔", // Subject line
+      html, // html body
+    },
+    (err) => {
+      if (err) {
+        console.log("err: ", err);
+      } else {
+        console.log("sent!");
+      }
     }
-  });
-}
+  );
+};
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ['--no-sandbox']
+    args: ["--no-sandbox"],
   });
   const page = await browser.newPage();
-  console.log('go to the page')
-  await page.goto('https://www.bestbuy.ca/en-ca/product/playstation-5-dualsense-wireless-controller-charging-station/14962276', { waitUntil: 'networkidle2' });
-
-  const [button] = await page.$x("//button[contains(., 'Add to Cart')][@disabled]");
-
-  
-
-  if (!button) {
-    const [activeButton] = await page.$x("//button[contains(., 'Add to Cart')]");
-    if (activeButton) {
-      sendEmail()
-      console.log('BUY IT NOW!!!')
-    }
-  } else {
-    console.log('button not enabled')
-  }
-
+  console.log("go to the page");
+  await page.goto(
+    "https://www.hermes.com/ca/en/category/women/bags-and-small-leather-goods/bags-and-clutches/#|",
+    { waitUntil: "networkidle2" }
+  );
+  const bagsRef = await page.$$(".product-item-name");
+  const bags = await Promise.all(
+    bagsRef.map((elRef) =>
+      page.evaluate((element) => element.textContent, elRef)
+    )
+  );
+  const wishList = ["Lindy", "Trim"];
+  const hitList = [];
+  wishList.forEach((name: string) => {
+    bags.forEach((bag: string) => {
+      if(bag.toLowerCase().includes(name.toLowerCase())){
+        hitList.push(bag);
+      }
+    });
+  });
+  console.log(hitList);
   await browser.close();
+  if (!hitList) return;
+
+  // write up the email
+  const contentArr = ['<h1>Hit! These bags are ready!</h1>'];
+  hitList.forEach(el => {
+    contentArr.push(`<p>${el}</p>`)
+  });
+  sendEmail(contentArr.join(''));
 })();
